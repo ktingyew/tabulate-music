24e4d9cd8e5671c654b27ab5f7e08d1751232cd5
ktingyew <62835795+ktingyew@users.noreply.github.com>
2022-02-13 14:45:19 +0800

    minor logging change

--- src/tabulate_my_music.py
+++ src/tabulate_my_music.py
@@ -35,7 +35,7 @@ def handle_exception(exc_type, exc_value, exc_traceback):
     if issubclass(exc_type, KeyboardInterrupt):
         sys.__excepthook__(exc_type, exc_value, exc_traceback)
         return
-    logger.error("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
+    logger.critical("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
 
 sys.excepthook = handle_exception
 
